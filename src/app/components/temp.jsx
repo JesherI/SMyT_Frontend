584249dba917ebf9912a409a083d90b4a200643f
Jehsersin_09 <jeshermendieta@gmail.com>
2024-07-05 11:20:56 -0600

    prueba

---
+++ src/app/components/temp.jsx
@@ -0,0 +1,14 @@
+function Temp(){
+    return(
+        <div className="temp">
+            <h1>Informaón temporal</h1>
+            <ul>
+                <li>Temperatura: </li>
+                <li>Presión: </li>
+                <li>Humedad: </li>
+            </ul>
+        </div>
+    )
+} 
+
+export default Temp;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,11 @@ import Homepage from "./components/homepage"
 import Header from "./components/header"
 import Footer from "./components/footer"
 import Login from "./components/login/login"
+import Temp from "./components/temp"
 export default function Home() {
   return (
     <div>
+      <Temp></Temp>
       <Login></Login>
     </div>
   )
